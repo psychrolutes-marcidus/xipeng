7a8ae631d9a392765048cf87c327b56da1519319
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2025-09-09 11:48:33 +0200

    Everything is just optionalable

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -1,11 +1,34 @@
+use serde::Deserialize;
+
 use chrono::prelude::*;
 
-struct CsvData {
-    timestamp: NaiveDateTime,
-    type_of_mobile: String,
-    MMSI: u64,
-    latitude: f32,
-    longitude: f32,
-    nav_status: String,
 
+#[derive(Debug, Deserialize)]
+pub struct CsvData {
+    pub timestamp: NaiveDateTime,
+    pub type_of_mobile: String,
+    pub mmsi: u64,
+    pub latitude: f64,
+    pub longitude: f64,
+    pub nav_status: String,
+    pub rot: Option<f64>,
+    pub sog: Option<f64>,
+    pub cog: Option<f64>,
+    pub heading: Option<u16>,
+    pub imo: String,
+    pub callsign: String,
+    pub name: Option<String>,
+    pub ship_type: String,
+    pub cargo_type: Option<String>,
+    pub width: Option<u16>,
+    pub length: Option<u16>,
+    pub position_fixing_device: String,
+    pub draught: Option<f64>,
+    pub destination: String,
+    pub eta: Option<NaiveDateTime>,
+    pub data_source_type: String,
+    pub a: Option<u16>,
+    pub b: Option<u16>,
+    pub c: Option<u16>,
+    pub d: Option<u16>,
 }
