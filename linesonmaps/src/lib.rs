7dd4017fec449e469b4b538b99063e5e61e561ce
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2025-09-10 09:51:09 +0200

    bump

---
+++ linesonmaps/src/lib.rs
@@ -0,0 +1,69 @@
+pub mod linestring;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytemuck::bytes_of;
+    use geo_traits::GeometryType;
+    use geo_traits::*;
+    use wkb::reader::read_wkb;
+
+    #[test]
+    fn three_dimensional() {
+        // https://en.wikipedia.org/wiki/Well-known_text_representation_of_geometry
+        const X: f64 = 1.0;
+        const XX: f64 = 1.1;
+        const Y: f64 = 2.0;
+        const YY: f64 = 2.2;
+        const M: f64 = 4.0;
+        const MM: f64 = 4.4;
+        const NDR: u8 = 1;
+        const LINESTRINGM: u32 = 2002;
+
+        // const _: () = assert_eq!(1,2);
+        const NDR_LE: [u8; 1] = NDR.to_le_bytes();
+        const LINESTRINGM_LE: [u8; 4] = LINESTRINGM.to_le_bytes();
+        const X_LE: [u8; 8] = X.to_le_bytes();
+        const XX_LE: [u8; 8] = XX.to_le_bytes();
+        const Y_LE: [u8; 8] = Y.to_le_bytes();
+        const YY_LE: [u8; 8] = YY.to_le_bytes();
+        const M_LE: [u8; 8] = M.to_le_bytes();
+        const MM_LE: [u8; 8] = MM.to_le_bytes();
+        // println!("{NDR_LE:?}\n{LINESTRINGM_LE:?}\n{X_LE:?}\n{Y_LE:?}\n{M_LE:?}");
+        let wkb = [
+            NDR_LE.to_vec(),
+            LINESTRINGM_LE.to_vec(),
+            X_LE.to_vec(),
+            Y_LE.to_vec(),
+            M_LE.to_vec(),
+            XX_LE.to_vec(),
+            YY_LE.to_vec(),
+            MM_LE.to_vec(),
+        ]
+        .concat();
+        let parsed = read_wkb(&wkb).unwrap();
+        assert_eq!(
+            parsed.geometry_type(),
+            wkb::reader::GeometryType::LineString
+        );
+        assert_eq!(parsed.dim(), Dimensions::Xym);
+        let t = match parsed.as_type() {
+            GeometryType::LineString(ls) => ls,
+            _ => unreachable!(),
+        };
+        println!("{0:?}",t.coord(0).unwrap());
+        assert_eq!(t.num_coords(), 2); //! does not quite work for some reason
+        // assert_eq!(parsed.num)
+
+        // assert!(false);
+    }
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ linesonmaps/src/linestring.rs
@@ -0,0 +1,228 @@
+use geo_traits::{
+    CoordTrait, GeometryTrait, GeometryType, LineStringTrait, PointTrait, UnimplementedGeometryCollection, UnimplementedLine, UnimplementedLineString, UnimplementedMultiLineString, UnimplementedMultiPoint, UnimplementedMultiPolygon, UnimplementedPolygon, UnimplementedRect, UnimplementedTriangle
+};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct CoordM {
+    pub x: f64,
+    pub y: f64,
+    pub m: f64,
+}
+
+impl CoordM {}
+
+impl CoordTrait for CoordM {
+    type T = f64;
+
+    fn dim(&self) -> geo_traits::Dimensions {
+        geo_traits::Dimensions::Xym
+    }
+
+    fn x(&self) -> Self::T {
+        self.x
+    }
+
+    fn y(&self) -> Self::T {
+        self.y
+    }
+
+    fn nth_or_panic(&self, n: usize) -> Self::T {
+        match n {
+            0 => self.x,
+            1 => self.y,
+            2 => (self.m),
+            e => panic!("tried to access dimension {e} in 3-dimensional coordinate"),
+        }
+    }
+}
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct PointM {
+    pub coord: CoordM,
+}
+
+impl PointM {}
+
+//wth is this
+impl GeometryTrait for PointM {
+    type T = f64;
+
+    type PointType<'a>
+        = PointM
+    where
+        Self: 'a;
+
+    type LineStringType<'a>
+        = UnimplementedLineString<Self::T>
+    where
+        Self: 'a;
+
+    type PolygonType<'a>
+        = UnimplementedPolygon<Self::T>
+    where
+        Self: 'a;
+
+    type MultiPointType<'a>
+        = UnimplementedMultiPoint<Self::T>
+    where
+        Self: 'a;
+
+    type MultiLineStringType<'a>
+        = UnimplementedMultiLineString<Self::T>
+    where
+        Self: 'a;
+
+    type MultiPolygonType<'a>
+        = UnimplementedMultiPolygon<Self::T>
+    where
+        Self: 'a;
+
+    type GeometryCollectionType<'a>
+        = UnimplementedGeometryCollection<Self::T>
+    where
+        Self: 'a;
+
+    type RectType<'a>
+        = UnimplementedRect<Self::T>
+    where
+        Self: 'a;
+
+    type TriangleType<'a>
+        = UnimplementedTriangle<Self::T>
+    where
+        Self: 'a;
+
+    type LineType<'a>
+        = UnimplementedLine<Self::T>
+    where
+        Self: 'a;
+
+    fn dim(&self) -> geo_traits::Dimensions {
+        geo_traits::Dimensions::Xym
+    }
+
+    fn as_type(
+        &self,
+    ) -> geo_traits::GeometryType<
+        '_,
+        Self::PointType<'_>,
+        Self::LineStringType<'_>,
+        Self::PolygonType<'_>,
+        Self::MultiPointType<'_>,
+        Self::MultiLineStringType<'_>,
+        Self::MultiPolygonType<'_>,
+        Self::GeometryCollectionType<'_>,
+        Self::RectType<'_>,
+        Self::TriangleType<'_>,
+        Self::LineType<'_>,
+    > {
+        geo_traits::GeometryType::Point(self)
+        // geo_traits::GeometryType::
+    }
+}
+
+impl PointTrait for PointM {
+    type CoordType<'a>
+        = CoordM
+    where
+        Self: 'a;
+
+    fn coord(&self) -> Option<Self::CoordType<'_>> {
+        Some(self.coord)
+    }
+}
+
+#[derive(Debug,Clone,PartialEq)]
+pub struct LineStringM(Vec<CoordM>);
+
+impl LineStringTrait for LineStringM{
+    type CoordType<'a> = CoordM
+    where
+        Self: 'a;
+
+    fn num_coords(&self) -> usize {
+        self.0.len()
+    }
+
+    unsafe fn coord_unchecked(&self, i: usize) -> Self::CoordType<'_> {
+        match i <= self.0.len() {
+            true => self.0[i],
+            false => panic!("u sux"), //TODO: better error message
+        }
+    }
+}
+
+impl GeometryTrait for LineStringM{
+    type T = f64;
+
+    type PointType<'a>
+        = PointM
+    where
+        Self: 'a;
+
+    type LineStringType<'a>
+        = LineStringM
+    where
+        Self: 'a;
+
+    type PolygonType<'a>
+        = UnimplementedPolygon<Self::T>
+    where
+        Self: 'a;
+
+    type MultiPointType<'a>
+        = UnimplementedMultiPoint<Self::T>
+    where
+        Self: 'a;
+
+    type MultiLineStringType<'a>
+        = UnimplementedMultiLineString<Self::T>
+    where
+        Self: 'a;
+
+    type MultiPolygonType<'a>
+        = UnimplementedMultiPolygon<Self::T>
+    where
+        Self: 'a;
+
+    type GeometryCollectionType<'a>
+        = UnimplementedGeometryCollection<Self::T>
+    where
+        Self: 'a;
+
+    type RectType<'a>
+        = UnimplementedRect<Self::T>
+    where
+        Self: 'a;
+
+    type TriangleType<'a>
+        = UnimplementedTriangle<Self::T>
+    where
+        Self: 'a;
+
+    type LineType<'a>
+        = UnimplementedLine<Self::T>
+    where
+        Self: 'a;
+
+    fn dim(&self) -> geo_traits::Dimensions {
+        geo_traits::Dimensions::Xym
+    }
+
+    fn as_type(
+        &self,
+    ) -> geo_traits::GeometryType<
+        '_,
+        Self::PointType<'_>,
+        Self::LineStringType<'_>,
+        Self::PolygonType<'_>,
+        Self::MultiPointType<'_>,
+        Self::MultiLineStringType<'_>,
+        Self::MultiPolygonType<'_>,
+        Self::GeometryCollectionType<'_>,
+        Self::RectType<'_>,
+        Self::TriangleType<'_>,
+        Self::LineType<'_>,
+    > {
+        GeometryType::LineString(self)
+    }
+}
