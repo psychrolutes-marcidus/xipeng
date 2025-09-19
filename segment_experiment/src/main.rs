5b02f01a7d50befec8a0298490b13602a64465f7
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2025-09-19 10:09:44 +0200

    bump

--- segment_experiment/src/main.rs
+++ segment_experiment/src/main.rs
@@ -6,7 +6,9 @@ use linesonmaps::types::*;
 use rayon::prelude::*;
 
 type LineString = LineStringM<4326>;
+type MMSI = i64;
 
+// output: segmented linestrings (with MMSI), number of segments, average length of segments all across different time parameters
 fn main() {
     let linestrings: Vec<LineString> = vec![];
 
@@ -17,13 +19,42 @@ fn main() {
             let c = THRESHOLDS.map(|t| segment_linestring(ls.clone(), |f, s| time_dist(f, s, t)));
             c
         })
-        .inspect(|measures| {
-            println!(
-                "segments created = {0:?}",
-                measures.iter().map(|mls| mls.num_line_strings())
+        // .inspect(|measures| {
+        //     println!(
+        //         "segments created = {0:?}\t average length = {1:?}",
+        //         measures
+        //             .iter()
+        //             .map(|mls| mls.num_line_strings())
+        //             .collect::<Vec<_>>(),
+        //         measures
+        //             .iter()
+        //             .map(|mls| mls
+        //                 .line_strings()
+        //                 .map(|ls| ls.0.len())
+        //                 .fold(0, |acc, x| acc + x)
+        //                 / mls.num_line_strings())
+        //             .collect::<Vec<_>>()
+        //     )
+        // })
+        .map(|meas| {
+            format!(
+                "segments created = {0:?}\t average length = {1:?}\n",
+                meas.iter()
+                    .map(|mls| mls.num_line_strings())
+                    .collect::<Vec<_>>(),
+                meas.iter()
+                    .map(|mls| mls
+                        .line_strings()
+                        .map(|ls| ls.0.len())
+                        .fold(0, |acc, x| acc + x)
+                        / mls.num_line_strings())
+                    .collect::<Vec<_>>()
             )
         })
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>().concat();
+        let p = "segment_experiment_results.txt";
+        std::fs::write(p, collected.as_str()).unwrap();
+        println!("output experiment results to {0}",p);
 
     println!("Hello, world!");
 }
