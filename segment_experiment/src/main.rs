bc141a83483eb4a91a607251bf0d28818ec0521c
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2025-09-25 08:28:17 +0200

    now the program can actually perform experiments

--- segment_experiment/src/main.rs
+++ segment_experiment/src/main.rs
@@ -1,12 +1,12 @@
 use chrono::DateTime;
-use data::{loaders::database::DbConn, tables::Ships, tables::trajectories::Trajectories};
+use data::loaders::database::DbConn;
 use dotenvy::*;
-use geo_traits::*;
+use geo::Distance;
 use linesonmaps::algo::segmenter::{TrajectorySplit, segment_linestring, segmenter};
 use linesonmaps::types::linestringm::LineStringM;
 use linesonmaps::types::pointm::PointM;
-use linesonmaps::types::*;
 use rayon::prelude::*;
+
 
 type LineString = LineStringM<4326>;
 type MMSI = i32;
@@ -31,7 +31,6 @@ fn main() {
         .zip(linestrings.trajectory)
         .filter(|p| p.0.to_string().len() == 9)
         .map(|(mmsi, ls)| (mmsi, LineStringM::<4326>(ls.0.into_iter().collect()))) //TODO: remove
-        .take(10)
         .collect();
 
     let _ = linestrings
@@ -40,14 +39,17 @@ fn main() {
             assert!(f.1.0.is_sorted_by_key(|k| k.m), "mmsi ={}", f.0);
         })
         .collect::<Vec<_>>();
-    dbg!(linestrings.first().unwrap().0);
+    // dbg!(linestrings.first().unwrap().0);
     // panic!();
     const THRESHOLDS: [f64; 7] = [15., 30., 60., 75., 90., 105., 120.];
-    let header = "mmsi, total_len, time_threshold, num_splits, avg_subtraj_len\n";
+    // let thresholds = (1..).map(|e| (e*10) as f64).take_while(|n|*n<300.).collect::<Vec<_>>(); //time
+    let thresholds = (2..).map(|e: i32| e.pow(2) as f64).take_while(|n|*n<=2500.).collect::<Vec<_>>(); //dist
+
+    let header = "mmsi,total_len,time_threshold,num_splits,avg_subtraj_len\n";
     let collected = linestrings
         .par_iter()
         .map(|(mmsi, ls)| {
-            let c = THRESHOLDS.map(|t| segmenter(ls.clone(), |f, s| time_dist(f, s, t)));
+            let c = thresholds.iter().map(|t| segmenter(ls.clone(), |f, s| time_dist(f, s, *t))).collect::<Vec<_>>();
             (mmsi, c)
         })
         .map(|(mmsi, measures)| {
@@ -58,7 +60,7 @@ fn main() {
             let rows = measures.into_iter().enumerate().map(|(idx, m)| {
                 format!(
                     "{mmsi},{total_len},{0},{1},{2}\n",
-                    THRESHOLDS[idx],
+                    thresholds[idx],
                     m.len(),
                     TrajectorySplit::concat_to_linestring(m.clone())
                         .unwrap()
@@ -69,29 +71,7 @@ fn main() {
             }).collect::<Vec<_>>().concat();
             rows
         }).collect::<Vec<_>>().concat();
-        // .map(|(mmsi, meas)| {
-        //     format!(
-        //         "MMSI={2}\t time intervals = {3:?} segments created = {0:?}\t average length = {1:?}\t total length = {4}\n",
-        //         meas.iter()
-        //             .map(|split| split.len())
-        //             .collect::<Vec<_>>(),
-        //         meas.iter()
-        //             .map(|split| split
-        //                 .iter()
-        //                 // .map(|ls| ls.0.len())
-        //                 .map(|s|{match s {
-        //                     TrajectorySplit::Point(p) => {1},
-        //                     TrajectorySplit::SubTrajectory(sls) => {sls.0.len()}
-        //                 }})
-        //                 .fold(0, |acc, x| acc + x)
-        //                 / split.len())
-        //             .collect::<Vec<_>>(),
-        //         mmsi
-        //     ,THRESHOLDS, TrajectorySplit::concat_to_linestring(meas[0].clone()).unwrap().0.len())
-        // })
-        // .collect::<Vec<_>>();
-    // .concat();
-    let p = "segment_experiment_results.csv";
+    let p = "segment_experiment_dist_results.csv";
     let collected = format!("{header}{collected}");
     std::fs::write(p, collected.as_str()).unwrap();
     println!("output experiment results to {0}", p);
@@ -99,4 +79,9 @@ fn main() {
 
 const fn time_dist(first: PointM, second: PointM, thres: f64) -> bool {
     second.coord.m - first.coord.m < thres
+}
+
+fn dist(first: PointM,second: PointM,thres: f64) -> bool {
+    use geo::algorithm::line_measures::metric_spaces::Geodesic;
+    Geodesic.distance(first,second) < thres
 }
