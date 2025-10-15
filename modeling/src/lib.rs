17bdced8c3892029ec099383da920129c2e37e71
razwaz <93257877+razwaz@users.noreply.github.com>
2025-10-15 09:55:44 +0200

    begin adding time

--- modeling/src/lib.rs
+++ modeling/src/lib.rs
@@ -13,14 +13,35 @@ mod tests {
 
     #[test]
     fn dumb_test() {
-        let line = Line::new(coord! { x: 0., y: 0. }, coord! { x: 5., y: 0. });
+        let line = Line::new(coord! { x: 8.0, y: 56.0 }, coord! { x: 8.2, y: 56.2 });
         /*let coords: Vec<CoordM<4326>> = [(1.0, 2.0, 0.0), (5.0, 3.0, 1.0), (3.0, 4.0, 2.0)]
             .map(|f| f.into())
             .to_vec();
         let first_line = LineM::from((coords[0],coords[1]));
         */
 
-        let a = line_to_aabb_triangles(&line, 1.0,1.0,5.0,1.0);
+        let a = line_to_aabb_triangles(&line, 1.0,1.0,10000000.0,10000000.0);
         dbg!(a);
+    }
+
+    #[test]
+    fn dumber_test() {
+        use geo::Point;
+        use linesonmaps::types::pointm::PointM;
+use geo::{Distance, Geodesic};
+
+// New York City
+let new_york_city = PointM::<4326>::from((-74.006, 40.7128, 1.0));
+
+// London
+let london = PointM::<4326>::from((-0.1278, 51.5074, -1.0));
+
+let distance = Geodesic.distance(new_york_city, london);
+dbg!(distance);
+
+assert_eq!(
+    5_585_234., // meters
+    distance.round()
+);
     }
 }
--- modeling/src/modeling.rs
+++ modeling/src/modeling.rs
@@ -1,4 +1,4 @@
-use geo::coord;
+use geo::{coord, point, Distance, InterpolatePoint};
 use geo_traits::{CoordTrait, GeometryTrait, LineTrait};
 
 pub fn line_to_aabb_triangles<Line:LineTrait + std::fmt::Debug>(line: &Line, a: f64, b: f64, c: f64, d: f64) -> Vec<geo_types::Triangle> 
@@ -7,18 +7,48 @@ where
 {
     let dx = line.end().x() - line.start().x();
     let dy = line.end().y() - line.start().y();
-    let dnorm = f64::sqrt(dx*dx+dy*dy);
-    let ndx = dx/dnorm;
-    let ndy = dy/dnorm;
-    let vec_orth_c = vec![-ndy*c, ndx*c]; // vec_orth_c/d project their length orthogonally along the normal vector of the line by the given lengths c,d respectively
-    let vec_orth_d = vec![ndy*d, -ndx*d];
-    let c_coord = coord![x: line.start().x()+vec_orth_c.first().unwrap(), y: line.start().y()+vec_orth_c.last().unwrap()]; // coordinate of left (port) of line AABB from line start
-    let d_coord = coord![x: line.start().x()+vec_orth_d.first().unwrap(), y: line.start().y()+vec_orth_d.last().unwrap()]; // coordinate of right (startboard) of line AABB from line start
-    let c_coord_end = coord![x: line.end().x()+vec_orth_c.first().unwrap(), y: line.end().y()+vec_orth_c.last().unwrap()]; // --||-- line end
-    let d_coord_end = coord![x: line.end().x()+vec_orth_d.first().unwrap(), y: line.end().y()+vec_orth_d.last().unwrap()]; // --||-- line end
+    //let dnorm = f64::sqrt(dx*dx+dy*dy);
+
+    //let vec_orth_c = vec![-dy, dx]; // orthogonal vector of the line, representative of c width
+    //let vec_orth_d = vec![dy, -dx]; // orthogonal vector of the line, representative of d width
+
+    let start_point_c = geo::algorithm::line_measures::metric_spaces::Geodesic.point_at_distance_between(
+        point!(line.start().x_y()),
+        point!(x: line.start().x()+(-dy), y: line.start().y()+(dx)),
+        c);
+
+    let start_point_d = geo::algorithm::line_measures::metric_spaces::Geodesic.point_at_distance_between(
+        point!(line.start().x_y()),
+        point!(x: line.start().x()+(dy), y: line.start().y()+(-dx)),
+        d);
+    
+    let end_point_c = geo::algorithm::line_measures::metric_spaces::Geodesic.point_at_distance_between(
+        point!(line.end().x_y()),
+        point!(x: line.end().x()+(-dy), y: line.end().y()+(dx)),
+        c);
+
+    let end_point_d = geo::algorithm::line_measures::metric_spaces::Geodesic.point_at_distance_between(
+        point!(line.end().x_y()),
+        point!(x: line.end().x()+(dy), y: line.end().y()+(-dx)),
+        d);
+
+    let p1 = point!(x: 8.15, y: 56.1);
+    let x_1 = line.start().x();
+    let y_1 = line.start().y();
+    let x_2 = line.end().x();
+    let y_2 = line.end().y();
+    let top = f64::abs((y_2-y_1)*p1.x()-(x_2-x_1)*p1.y()+(x_2*y_1)-(y_2*x_1));
+    let bottom = f64::sqrt(f64::powi(y_2-y_1, 2)+f64::powi(x_2-x_1, 2));
+
+    let top2 = f64::abs((dy*p1.x())-(dx*p1.y())+(x_2*y_1)-(y_2*x_1));
+    dbg!(top);
+    dbg!(top2);
+    let dist_from_point_to_line = (top/bottom);
+    dbg!(dist_from_point_to_line);
 
     vec![
-        geo_types::geometry::Triangle::new(c_coord, d_coord, c_coord_end),
-        geo_types::geometry::Triangle::new(d_coord, c_coord_end, d_coord_end)
+        geo_types::geometry::Triangle::new(start_point_c.0, start_point_d.0, end_point_c.0),
+        geo_types::geometry::Triangle::new(start_point_d.0, end_point_c.0, end_point_d.0)
     ]
+
 }
