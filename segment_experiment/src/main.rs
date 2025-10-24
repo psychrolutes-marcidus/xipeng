ae64ef6657bb6759d885d7e79ef577c46cce19fc
Anders Rask Sørensen <61500734+Uewotm90@users.noreply.github.com>
2025-10-24 11:05:29 +0200

    2-linestring-strategy-experiment (#17)
    * added new dependency
    
    * bump
    
    * updated filename
    
    * savepoint
    
    * now every geometry type has associated CRS
    
    * bump
    
    * EPSG:4326 is now default CRS for geometry types, so it does not need to be annotated at all times
    
    * all distance measurements
    
    * mer test
    
    * remove vscode related files
    
    * bump
    
    * removed old comments/code
    
    * removed old comments, again
    
    * added le funny linestring, for testing purposes
    
    * added a test that exposed my code as broken
    
    * fmt
    
    * added new type
    
    * added new trait derive
    
    * more vscode files
    
    * refactored segmenter function
    
    * now it compiles on release :))
    
    * moar testing
    
    * moar boilerplate
    
    * bump
    
    * væk med det gamle
    
    * does not need to be in test
    
    * clippy fix pt 1
    
    * clippy pt 2
    
    * we do a little documentation + removed useless comments/code
    
    * comments
    
    * removed dependency that was no longer in use
    
    * changed return type to `MultiLineStringM` and changed corresponding test code
    
    * bump
    
    * bump
    
    * bump
    
    * now with mmsi
    
    * bump
    
    * bump
    
    * added filter for 0 length linestrings, might be redundant
    
    * bump
    
    * bump
    
    * updated trajectory
    
    * implemented new segmenting algorithm
    
    * it works, kinda, maybe
    
    * now with csv
    
    * now the program can actually perform experiments
    
    * updated .gitignore to ignore experimental results
    
    * small fix
    
    * væk med gammel skidt
    
    * no need to enumerate anymore
    
    * removed old and useless things + added some comments
    
    * some clippy lints
    
    * Update .gitignore
    
    noice
    
    Co-authored-by: Rasmus Hald Nielsen <37369606+Murmeldyret@users.noreply.github.com>
    
    * bump
    
    * tihi
    
    * implemented sql batching iterator (maybe?), awaiting tests
    
    * added derive
    
    * new function, lazily get trajectories
    
    * remove old comments
    
    * a little warning
    
    * changed iterator to use prepared statement (could have a performance benefit with repeated calls)
    
    * removed old sql str
    
    * added dependency for writing geometrys to DB
    
    * added to_wkb for trajectory split
    
    * added error variant
    
    * might work
    
    * i think it works
    
    * bump
    
    * new bulk inserter
    
    * nu mangler der bare tests :)))
    
    * timestamp + interval splits
    
    * fixed string formatting
    
    * added new test case, that showcases i dont know boolean algebra
    
    * fixed split function
    
    * xd
    
    * bump
    
    * fjern ubruglig test
    
    * bump
    
    * re-review ready
    
    * bump
    
    ---------
    
    Co-authored-by: Rasmus Hald Nielsen <37369606+Murmeldyret@users.noreply.github.com>

---
+++ segment_experiment/src/main.rs
@@ -0,0 +1,99 @@
+use chrono::DateTime;
+use data::loaders::database::DbConn;
+use dotenvy::*;
+use geo::Distance;
+use linesonmaps::algo::segmenter::{TrajectorySplit, segmenter};
+use linesonmaps::types::linestringm::LineStringM;
+use linesonmaps::types::pointm::PointM;
+use rayon::prelude::*;
+
+type LineString = LineStringM<4326>;
+#[allow(clippy::upper_case_acronyms)]
+type MMSI = i32;
+
+// output: segmented linestrings (with MMSI), number of segments, average length of segments all across different time parameters
+fn main() {
+    dotenv().expect("failed to load environment variables");
+    let mut conn = DbConn::new().expect("failed to establish database connection");
+
+    let from = DateTime::parse_from_str("2024-01-01 00:00:00 +0000", "%Y-%m-%d %H:%M:%S%.3f %z")
+        .expect("failed to parse date-string");
+    let to = DateTime::parse_from_str("2024-01-02 00:00:00 +0000", "%Y-%m-%d %H:%M:%S%.3f %z")
+        .expect("failed to parse date-string");
+
+    let crap = conn
+        .fetch_data(from.into(), to.into())
+        .expect("failed to query database");
+
+    let linestrings = crap.trajectories;
+
+    let linestrings: Vec<(MMSI, LineString)> = linestrings
+        .mmsi
+        .into_iter()
+        .zip(linestrings.trajectory)
+        .filter(|p| p.0.to_string().len() == 9)
+        .map(|(mmsi, ls)| (mmsi, LineStringM::<4326>(ls.0.into_iter().collect()))) //TODO: remove
+        .collect();
+
+    let _ = linestrings
+        .iter()
+        .inspect(|f| {
+            assert!(f.1.0.is_sorted_by_key(|k| k.m), "mmsi ={}", f.0);
+        })
+        .collect::<Vec<_>>();
+    // let thresholds = (1..).map(|e| (e*10) as f64).take_while(|n|*n<300.).collect::<Vec<_>>(); //time
+    let thresholds = (2..)
+        .map(|e: i32| 2_i32.pow(e as u32) as f64)
+        .take_while(|n| *n <= 2500.)
+        .collect::<Vec<_>>(); //dist
+
+    let header = "mmsi,total_len,time_threshold,num_splits,avg_subtraj_len\n";
+    let collected = linestrings
+        .par_iter()
+        .map(|(mmsi, ls)| {
+            let c = thresholds
+                .iter()
+                .map(|t| segmenter(ls.clone(), |f, s| dist(f, s, *t)))
+                .collect::<Vec<_>>();
+            (mmsi, c)
+        })
+        .map(|(mmsi, measures)| {
+            let total_len = TrajectorySplit::concat_to_linestring(measures[0].clone())
+                .expect("Linestrings from database should be valid, so should this")
+                .0
+                .len();
+            measures
+                .into_iter()
+                .enumerate()
+                .map(|(idx, m)| {
+                    format!(
+                        "{mmsi},{total_len},{0},{1},{2}\n",
+                        thresholds[idx],
+                        m.len(),
+                        TrajectorySplit::concat_to_linestring(m.clone())
+                            .expect("if database trajectories are valid, these should be as well")
+                            .0
+                            .len()
+                            / m.len()
+                    )
+                })
+                .collect::<Vec<_>>()
+                .concat()
+        })
+        .collect::<Vec<_>>()
+        .concat();
+    let p = "segment_experiment_dist_results.csv";
+    let collected = format!("{header}{collected}");
+    std::fs::write(p, collected.as_str()).expect("failed to write");
+    println!("output experiment results to {0}", p);
+}
+
+#[allow(unused)]
+const fn time_dist(first: PointM, second: PointM, thres: f64) -> bool {
+    second.coord.m - first.coord.m < thres
+}
+
+fn dist(first: PointM, second: PointM, thres: f64) -> bool {
+    use geo::algorithm::line_measures::metric_spaces::Geodesic;
+    Geodesic.distance(first, second) < thres
+}
